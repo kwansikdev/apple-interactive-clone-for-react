93b086ac87664b7eaba94faa11052eb7906d70f7
kwansikdev <kwansikdev@gmail.com>
2021-11-07 14:28:58 +0900

    feat: Set initial settings

---
+++ src/pages/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const MainContainer = styled.div``;
+
+const Main = () => {
+  return <MainContainer>Main</MainContainer>;
+};
+export default Main;
